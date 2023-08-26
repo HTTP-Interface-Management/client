b1e3b528f0cabb9875543f3b5690d797f0347961
mfliao <140330719+mfliao@users.noreply.github.com>
2023-08-26 23:52:44 +0800

    Revert "新建项目功能"

--- src/components/create-interface/index.vue
+++ src/components/create-interface/index.vue
@@ -1,114 +1,124 @@
 <template>
-  <a-tabs default-active-key="1">
-    <a-tab-pane key="1" title="手动新建">
-      <div class="check">
-        <div class="interface-data">
-          <div class="items">
-            <h3>接口功能描述</h3>
-            <a-input />
-          </div>
-          <div class="items">
-            <h3>接口地址（URL）</h3>
-            <a-input />
-          </div>
-          <div class="items">
-            <h3>支持格式</h3>
-            <a-input />
-          </div>
-          <div class="items">
-            <h3>HTTP请求方式</h3>
-            <a-select :style="{width:'320px'}" placeholder="Select A Request method" allow-create>
-              <a-option >GET</a-option>
-              <a-option>POST</a-option>
-              <a-option>PUT</a-option>
-              <a-option>DELETE</a-option>
-              <a-option>HEAD</a-option>
-              <a-option>CONNECT</a-option>
-              <a-option>OPTIONS</a-option>
-              <a-option>TRACE</a-option>
-              <a-option>PATCH</a-option>
-            </a-select>
-          </div>
-          <div class="items">
-            <h3>请求参数</h3>
-            <a-radio-group 
-              type="button" 
-              @change="">
-              <a-radio value="request-headers">Request Headers</a-radio>
-              <a-radio value="request-query">Requuest Query</a-radio>
-              <a-radio value="request-body">Request Body</a-radio>
-            </a-radio-group>  
-
-            <a-button 
-              type="primary"
-              shape="round"
-              style="margin-left: 340px" 
-              @click=""> Add
-            </a-button>  
-
-            <a-table 
-              :data="requestdata" 
-            >     
-              <template #columns>
-                <a-table-column title="参数" data-index="param" slotName="param">               
-                </a-table-column>
-                <a-table-column title="类型" data-index="types" slotName="types">  
-                </a-table-column>
-                <a-table-column title="必选" data-index="required" slotName="required">              
-                </a-table-column>
-                <a-table-column title="默认参数" data-index="defaultparam" slotName="defaultparam">          
-                </a-table-column>
-                <a-table-column title="说明" data-index="illustrate" slotName="illustrate">      
-                </a-table-column>
-                <a-table-column title="操作" data-index="operate" slotName="operate">
-                </a-table-column>
-              </template> 
-            </a-table>      
-          </div>
-          
-          <div class="items">
-            <h3>返回数据</h3>
-
-            <a-radio-group type="button">
-              <a-radio value="request-headers">Request Headers</a-radio>
-              <a-radio value="request-body">Request Body</a-radio>
-              <a-radio value="example">example</a-radio>
-            </a-radio-group> 
-
-            <a-button 
-              type="primary"
-              shape="round"
-              style="margin-left: 390px" 
-              @click=""> Add
-            </a-button>  
-
-            <a-table 
-              :data="responsedata" 
-            >
-              <template #columns>
-                <a-table-column title="参数" data-index="param"></a-table-column>
-                <a-table-column title="类型" data-index="types"></a-table-column>
-                <a-table-column title="必选" data-index="required"></a-table-column>
-                <a-table-column title="默认参数" data-index="defaultparam"></a-table-column>
-                <a-table-column title="说明" data-index="illustrate"></a-table-column>
-                <a-table-column title="操作" data-index="operate"></a-table-column>
-              </template>
-            </a-table>   
-          </div> 
-        </div>
+  <div class="check">
+    <div class="interfacedata">
+      <div class="items">
+        <h3>接口功能描述</h3>
+        <a-input />
       </div>
-    </a-tab-pane>
+      <div class="items">
+        <h3>接口地址（URL）</h3>
+        <a-input />
+      </div>
+      <div class="items">
+        <h3>支持格式</h3>
+        <a-input />
+      </div>
+      <div class="items">
+        <h3>HTTP请求方式</h3>
+        <a-select :style="{width:'320px'}" placeholder="Select A Request method" allow-create>
+          <a-option >GET</a-option>
+          <a-option>POST</a-option>
+          <a-option>PUT</a-option>
+          <a-option>DELETE</a-option>
+          <a-option>HEAD</a-option>
+          <a-option>CONNECT</a-option>
+          <a-option>OPTIONS</a-option>
+          <a-option>TRACE</a-option>
+          <a-option>PATCH</a-option>
+        </a-select>
+      </div>
+      <div class="items">
+        <h3>请求参数</h3>
+        <a-radio-group 
+          type="button" 
+          @change="">
+          <a-radio value="request-headers">Request Headers</a-radio>
+          <a-radio value="request-query">Requuest Query</a-radio>
+          <a-radio value="request-body">Request Body</a-radio>
+        </a-radio-group>  
 
-    <a-tab-pane key="2" title="swagger导入">
-      <a-space>
-        <a-upload 
-        draggable
-        :on-success="onSuccess"
-        action=""
-        />
-      </a-space>
-    </a-tab-pane>
-  </a-tabs>
+        <a-button 
+          type="primary"
+          shape="round"
+          style="margin-left: 340px" 
+          @click=""> Add
+        </a-button>  
+
+        <a-table 
+          :data="requestdata" 
+        >     
+          <template #columns>
+            <a-table-column title="参数" data-index="param" slotName="param">
+              <template #param="{ rowIndex }">
+                <a-input v-model="requestdata[rowIndex].param" />
+              </template>
+            </a-table-column>
+            <a-table-column title="类型" data-index="types" slotName="types">
+              <template #types="{ rowIndex }">
+                <a-input v-model="requestdata[rowIndex].types" />
+              </template>
+            </a-table-column>
+            <a-table-column title="必选" data-index="required" slotName="required">
+              <template #province="{ rowIndex }">
+                <a-select v-model="requestdata[rowIndex].required" >
+                  <a-option v-for="value of Object.keys(options)">{{value}}</a-option>
+                </a-select>
+              </template>
+            </a-table-column>
+            <a-table-column title="默认参数" data-index="defaultparam" slotName="defaultparam">
+              <template #defaultparam="{ rowIndex }">
+                <a-input v-model="requestdata[rowIndex].defaultparam" />
+              </template>
+            </a-table-column>
+            <a-table-column title="说明" data-index="illustrate" slotName="illustrate">
+              <template #illustrate="{ rowIndex }">
+                <a-input v-model="requestdata[rowIndex].illustrate" />
+              </template>
+            </a-table-column>
+              <a-table-column title="操作" data-index="operate" slotName="operate">
+                <template>
+
+                </template>
+              </a-table-column>
+          </template> 
+        </a-table>      
+      </div>
+      
+      <div class="items">
+        <h3>返回数据</h3>
+
+        <a-radio-group type="button">
+          <a-radio value="request-headers">Request Headers</a-radio>
+          <a-radio value="request-body">Request Body</a-radio>
+          <a-radio value="example">example</a-radio>
+        </a-radio-group> 
+
+        <a-button 
+          type="primary"
+          shape="round"
+          style="margin-left: 390px" 
+          @click=""> Add
+        </a-button>  
+
+        <a-table 
+          :data="responsedata" 
+        >
+          <template #columns>
+            <a-table-column title="参数" data-index="param"></a-table-column>
+            <a-table-column title="类型" data-index="types"></a-table-column>
+            <a-table-column title="必选" data-index="required"></a-table-column>
+            <a-table-column title="默认参数" data-index="defaultparam"></a-table-column>
+            <a-table-column title="说明" data-index="illustrate"></a-table-column>
+            <a-table-column title="操作" data-index="operate">
+              <template>
+                
+              </template>
+            </a-table-column>
+          </template>
+        </a-table>   
+      </div> 
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -132,13 +142,12 @@
       illustrate:''
     }]);
 
-    const onSuccess = () => {
+    const options = [true, false];
 
-    }
 </script>
 
 <style scoped lang="less">
-  .interface-data{
+  .interfacedata{
     margin: 0 100px;
   }
   .items{
